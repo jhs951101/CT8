d75d71ca9be83bd121f0fc444ba24898a540c921
spg1101 <sharping@SharPingui-MacBookPro.local>
2021-06-28 19:21:26 +0900

    m

---
+++ hw1.py
@@ -0,0 +1,14 @@
+﻿Bloods = []
+
+for i in range(0,10,1):
+    Bloods.append(input('헌열할 혈액형 (a, b, ab, o) : '))
+
+print('\n저장된 혈액형')
+for i in range(0,10,1):
+    print(Bloods[i], end='    ')
+
+print()
+print('A   혈액형 수 : ', Bloods.count('a'))
+print('B   혈액형 수 : ', Bloods.count('b'))
+print('AB 혈액형 수 : ', Bloods.count('ab'))
+print('O   혈액형 수 : ', Bloods.count('o'))
---
+++ hw2.py
@@ -0,0 +1,40 @@
+﻿mylist = []
+
+while True:
+    print('*'*19)
+    print('1. 이름 추가')
+    print('2. 이름 삭제')
+    print('3. 이름 수정')
+    print('4. 종료')
+    menu = input('메뉴 선택 : ')
+
+    if menu == '4':
+        break
+    elif menu >= '1' and menu <= '3':
+        name = input('이름 : ')
+        error = False
+
+        if menu == '1':
+            if mylist.count(name) >= 1:
+                print('이미 있는 이름')
+                error = True
+            else:
+                 mylist.append(name)
+        elif menu == '2' or menu == '3':
+            if mylist.count(name) <= 0:
+                print('해당 이름 없음')
+                error = True
+            else:
+                if menu == '2':
+                    mylist.remove(name)
+                elif menu == '3':
+                    newName = input('새 이름 : ')
+                    mylist[mylist.index(name)] = newName
+
+        if not error:
+            print(mylist)
+            
+    else:
+        print('메뉴에 없는 번호')
+
+    print()
